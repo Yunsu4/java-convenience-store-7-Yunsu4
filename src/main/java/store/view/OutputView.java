cab864df368214bf1e81f83ec38ba96aff4ee62a
Yunsu4 <ysa020428@gmail.com>
2024-11-11 00:01:32 +0900

    feat(view): 입출력 메서드 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -9,8 +9,17 @@ public class OutputView {
                 + "현재 보유하고 있는 상품입니다."+newline);
     }
 
-    public void printReceipt(){
+    public void printReceiptStart(){
+        System.out.println("==============W 편의점================"
+                +newline+"상품명        수량    금액");
+    }
 
+    public void startPrintBonusProduct(){
+        System.out.println("=============증    정===============");
+    }
+
+    public void printDividingLine(){
+        System.out.println("====================================");
     }
 
 
